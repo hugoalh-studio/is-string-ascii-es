54769155ab21843ba917295e2d1feeee2fcbef18
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-05-06 10:49:43 +0800

    Update test

--- mod.bench.ts
+++ mod.bench.ts
@@ -1,25 +1,55 @@
 import { isStringASCII, isStringASCIIControl, isStringASCIIPrintable } from "./mod.ts";
-Deno.bench("1", { permissions: "none" }, () => {
-	const sample = "日本語";
-	isStringASCII(sample);
-	isStringASCIIControl(sample);
-	isStringASCIIPrintable(sample);
+const sample1 = "日本語";
+const sample2 = "👀";
+const sample3 = "Hello, world!";
+const sample4 = "";
+const sample5 = `Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
+Et laoreet odio sanctus ea.
+Sea in dolores diam tincidunt labore sea stet vero dolor ut est.
+At aliquyam diam facilisis lorem et takimata et volutpat eros erat ipsum velit labore sed ea illum.
+Dolor lorem sed et volutpat exerci gubergren gubergren tempor quis ea eirmod eos ut dolor autem ipsum accumsan.`;
+Deno.bench("1 All", { permissions: "none" }, () => {
+	isStringASCII(sample1);
 });
-Deno.bench("2", { permissions: "none" }, () => {
-	const sample = "👀";
-	isStringASCII(sample);
-	isStringASCIIControl(sample);
-	isStringASCIIPrintable(sample);
+Deno.bench("1 Control", { permissions: "none" }, () => {
+	isStringASCIIControl(sample1);
 });
-Deno.bench("3", { permissions: "none" }, () => {
-	const sample = "Hello, world!";
-	isStringASCII(sample);
-	isStringASCIIControl(sample);
-	isStringASCIIPrintable(sample);
+Deno.bench("1 Printable", { permissions: "none" }, () => {
+	isStringASCIIPrintable(sample1);
 });
-Deno.bench("4", { permissions: "none" }, () => {
-	const sample = "";
-	isStringASCII(sample);
-	isStringASCIIControl(sample);
-	isStringASCIIPrintable(sample);
+Deno.bench("2 All", { permissions: "none" }, () => {
+	isStringASCII(sample2);
+});
+Deno.bench("2 Control", { permissions: "none" }, () => {
+	isStringASCIIControl(sample2);
+});
+Deno.bench("2 Printable", { permissions: "none" }, () => {
+	isStringASCIIPrintable(sample2);
+});
+Deno.bench("3 All", { permissions: "none" }, () => {
+	isStringASCII(sample3);
+});
+Deno.bench("3 Control", { permissions: "none" }, () => {
+	isStringASCIIControl(sample3);
+});
+Deno.bench("3 Printable", { permissions: "none" }, () => {
+	isStringASCIIPrintable(sample3);
+});
+Deno.bench("4 All", { permissions: "none" }, () => {
+	isStringASCII(sample4);
+});
+Deno.bench("4 Control", { permissions: "none" }, () => {
+	isStringASCIIControl(sample4);
+});
+Deno.bench("4 Printable", { permissions: "none" }, () => {
+	isStringASCIIPrintable(sample4);
+});
+Deno.bench("5 All", { permissions: "none" }, () => {
+	isStringASCII(sample5);
+});
+Deno.bench("5 Control", { permissions: "none" }, () => {
+	isStringASCIIControl(sample5);
+});
+Deno.bench("5 Printable", { permissions: "none" }, () => {
+	isStringASCIIPrintable(sample5);
 });
--- mod.test.ts
+++ mod.test.ts
@@ -1,26 +1,56 @@
 import { assertEquals } from "STD/assert/assert-equals";
 import { isStringASCII, isStringASCIIControl, isStringASCIIPrintable } from "./mod.ts";
-Deno.test("1", { permissions: "none" }, () => {
-	const sample = "日本語";
-	assertEquals(isStringASCII(sample), false);
-	assertEquals(isStringASCIIControl(sample), false);
-	assertEquals(isStringASCIIPrintable(sample), false);
+const sample1 = "日本語";
+const sample2 = "👀";
+const sample3 = "Hello, world!";
+const sample4 = "";
+const sample5 = `Wisi sed et at vero eos nostrud volutpat sed stet dignissim sit sanctus in eros.
+Et laoreet odio sanctus ea.
+Sea in dolores diam tincidunt labore sea stet vero dolor ut est.
+At aliquyam diam facilisis lorem et takimata et volutpat eros erat ipsum velit labore sed ea illum.
+Dolor lorem sed et volutpat exerci gubergren gubergren tempor quis ea eirmod eos ut dolor autem ipsum accumsan.`;
+Deno.test("1 All", { permissions: "none" }, () => {
+	assertEquals(isStringASCII(sample1), false);
 });
-Deno.test("2", { permissions: "none" }, () => {
-	const sample = "👀";
-	assertEquals(isStringASCII(sample), false);
-	assertEquals(isStringASCIIControl(sample), false);
-	assertEquals(isStringASCIIPrintable(sample), false);
+Deno.test("1 Control", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIControl(sample1), false);
 });
-Deno.test("3", { permissions: "none" }, () => {
-	const sample = "Hello, world!";
-	assertEquals(isStringASCII(sample), true);
-	assertEquals(isStringASCIIControl(sample), false);
-	assertEquals(isStringASCIIPrintable(sample), true);
+Deno.test("1 Printable", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIPrintable(sample1), false);
 });
-Deno.test("4", { permissions: "none" }, () => {
-	const sample = "";
-	assertEquals(isStringASCII(sample), true);
-	assertEquals(isStringASCIIControl(sample), true);
-	assertEquals(isStringASCIIPrintable(sample), true);
+Deno.test("2 All", { permissions: "none" }, () => {
+	assertEquals(isStringASCII(sample2), false);
+});
+Deno.test("2 Control", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIControl(sample2), false);
+});
+Deno.test("2 Printable", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIPrintable(sample2), false);
+});
+Deno.test("3 All", { permissions: "none" }, () => {
+	assertEquals(isStringASCII(sample3), true);
+});
+Deno.test("3 Control", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIControl(sample3), false);
+});
+Deno.test("3 Printable", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIPrintable(sample3), true);
+});
+Deno.test("4 All", { permissions: "none" }, () => {
+	assertEquals(isStringASCII(sample4), true);
+});
+Deno.test("4 Control", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIControl(sample4), true);
+});
+Deno.test("4 Printable", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIPrintable(sample4), true);
+});
+Deno.test("5 All", { permissions: "none" }, () => {
+	assertEquals(isStringASCII(sample5), true);
+});
+Deno.test("5 Control", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIControl(sample5), false);
+});
+Deno.test("5 Printable", { permissions: "none" }, () => {
+	assertEquals(isStringASCIIPrintable(sample5), false);
 });
